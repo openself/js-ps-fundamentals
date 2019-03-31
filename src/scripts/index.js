b7e9ccc2957d8bf29a21b4486e6878dc063b4ee7
openself <openself@bk.ru>
2019-03-31 11:02:18 +0300

    Start learning

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
-import '../styles/index.scss';
+function sendCars(day,  ...cars) {
+    cars.forEach(car => console.log(car))
+}
+sendCars('Monday', 1, 2)
 
-console.log('webpack starterkit');
+const carIds = [10, 20, 30, 40]
+let [carId1, carId2, ... restIds] = carIds
+console.log(carId1, carId2, restIds)
+let [, carId3, ... restIds2] = carIds
+console.log(carId3, restIds)
